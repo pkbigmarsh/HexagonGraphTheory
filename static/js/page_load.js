83ed647c128ea1483361e268c5cc30f0c29d3ecc
Marshall Sumwalt <marshallsumwalt@gmail.com>
2013-10-29 16:54:29 -0500

    Got the canvas set up.

---
+++ static/js/page_load.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var canvas = document.getElementById("grid_canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(0,0,150,75);
+});
