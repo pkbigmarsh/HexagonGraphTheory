db0de2c1c9cc8ad863bf5dfc37118f01739cb89a
Marshall Sumwalt <marshallsumwalt@gmail.com>
2013-11-08 17:48:58 -0600

    Added an opposite direction and throw_error function

--- static/js/core.js
+++ static/js/core.js
@@ -12,3 +12,16 @@ function truncate(number, digits)
 
     return truncatedNum / multiplier;
 }
+
+function opposite_direction(direction){
+	return (direction + 3) % 6;
+}
+
+function throw_error(source_function, message, data)
+{
+	source_function = default_arg(source_function, "throw_error");
+	message 		= default_arg(message, "No message supplied");
+	data 			= default_arg(data, {});
+
+	console.error("Function: %s\n\tMessage: %s\n\t\tData: %o", source_function, message, data);
+}
