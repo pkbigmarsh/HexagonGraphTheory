4941f41b31a674c5a969c63fee33fe7653b88292
Marshall Sumwalt <marshallsumwalt@gmail.com>
2013-11-08 18:04:00 -0600

    Added get_direction_to functions for both a point(x, y) and another HexVertex object.

--- static/js/HexVertex.js
+++ static/js/HexVertex.js
@@ -4,18 +4,46 @@ function HexVertex(parameters){
 	this.graphics 		= this.shape.graphics;
 	this.above 			= default_arg(parameters.above, null);
 	this.height 		= default_arg(parameters.height, 0);
+	this.neighbors 		= default_arg(parameters.neighbors, [null, null, null, null, null, null]);
+	this.below 			= default_arg(parameters.below, null);
 
 	this.set_x = function(new_x) {
 		this.shape.x = new_x;
 	};
 
+	this.get_x = function() {
+		return this.shape.x;
+	};
+
 	this.set_y = function(new_y) {
 		this.shape.y = new_y;
-	}
+	};
+
+	this.get_y = function() {
+		return this.shape.y;
+	};
 
 	this.set_pos = function(pos){
 		this.shape.x = pos.x;
 		this.shape.y = pos.y;
+	};
+
+	this.get_pos = function() {
+		return {x: this.shape.x, y: this.shape.y};
+	};
+
+	this.get_top = function() {
+		var current_hex = this;
+		while(current_hex.above != null)
+			current_hex = current_hex.above;
+		return current_hex;
+	};
+
+	this.get_bottom = function() {
+		var current_hex = this;
+		while(current_hex.below != null)
+			current_hex = current_hex.below;
+		return current_hex;
 	}
 
 	this.create_hex = function(color)
@@ -87,11 +115,11 @@ function HexVertex(parameters){
 			this.graphics.lineTo(points[i].x, points[i].y);
 		this.graphics.endStroke();
 		this.graphics.endFill();
-	}
+	};
 
 	this.addEventListener = function(event_str, event_function) {
 		this.shape.addEventListener(event_str, event_function);
-	}
+	};
 
 	this.clone = function() {
 		return new HexVertex({
@@ -99,7 +127,73 @@ function HexVertex(parameters){
 			above: this.above,
 			height: this.height
 		});
-	}
+	};
+
+	this.get_direction_to_point = function(end_pos) {
+		if(typeof end_pos.x == 'undefined' || typeof end_pos.y == 'undefined')
+		{
+			throw_error("get_direction_to_point", "Given point is missing x/y component", end_pos);
+			return null;
+		}	
+
+
+		var start_pos = this.get_pos();
+
+		var vertical_bounds = {
+			left: start_pos.x - HEX_RADIUS / 2, 
+			right: start_pos.x + HEX_RADIUS / 2
+		};
+		
+
+		if(start_pos.y > end_pos.y)
+		{
+			if(end_pos.x < vertical_bounds.left)
+				return NE;
+			else if(end_pos.x > vertical_bounds.right)
+				return NW;
+			else
+				return NN;
+		}
+		else
+		{
+			if(end_pos.x < vertical_bounds.left)
+				return SE;
+			else if(end_pos.x > vertical_bounds.right)
+				return SW;
+			else
+				return SS;
+		}
+	};
+
+	this.get_direction_to_hex = function(other_hex) {
+		if(other_hex == null)
+		{
+			throw_error("get_direction_to_hex", "Given Hex is empty", other_hex);
+			return null;
+		}
+		other_hex = other_hex.get_bottom();
+		var start_pos = this.get_pos();
+		var end_pos = other_hex.get();
+
+		if(start_pos.y > end_pos.y)
+		{
+			if(start_pos.x > end_pos.x)
+				return NE;
+			else if(start_pos.x == end_pos.x)
+				return NN;
+			else
+				return NW;
+		}
+		else
+		{
+			if(start_pos.x > end_pos.x)
+				return SE;
+			else if(start_pos.x == end_pos.x)
+				return SN;
+			else
+				return SW;
+		}
+	};
 
 	this.create_hex(parameters.color);
 }
