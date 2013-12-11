39459ff1f6f356c7068e2cf33825cdadf064d4f4
Marshall Sumwalt <marshallsumwalt@gmail.com>
2013-12-10 23:21:20 -0600

    ap done

--- static/js/algorithms/ap_algorithm.js
+++ static/js/algorithms/ap_algorithm.js
@@ -1,13 +1,22 @@
+var ap;
+var next;
+
+var selected_one = null;
+var selected_two = null;
+var current_highlighted = null;
+var placed_edges = [];
+var time = null;
+
 $("#all_pairs_button").on("click", function() {
 	disable_buttons();
-	ap_start();
-});
 
-function ap_start()
-{
 	place_placed_hexes_into_graph();
 	graph.clean();
-	var ap = new ap_struct(graph.num_vertices);
+	ap = new ap_struct(graph.num_vertices);
+	next = new ap_struct(graph.num_vertices, null);
+
+	for(var i = 0; i < graph.num_vertices; i ++)
+		ap.weight[i][i] = 0;
 
 	for(var i = 0; i < graph.num_vertices; i ++)
 	{
@@ -25,26 +34,112 @@ function ap_start()
 		}
 	}
 
-	console.log("Before");
-	print_ap(ap);
-
 	var through_k;
-	for(var k = 1; k < graph.num_vertices - 1; k ++)
+	for(var k = 0; k < graph.num_vertices; k ++)
 	{
-		for(var i = 1; i <= graph.num_vertices - 1; i ++)
+		for(var i = 0; i < graph.num_vertices; i ++)
 		{
-			for(var j = 1; j <= graph.num_vertices - 1; j ++)
+			for(var j = 0; j < graph.num_vertices; j ++)
 			{
-				// console.log(k, i, j);
 				through_k = ap.weight[i][k] + ap.weight[k][j];
 				if(through_k < ap.weight[i][j]) 
+				{
 					ap.weight[i][j] = through_k;
+					next.weight[i][j] = k;
+				}
 			}
 		}
 	}
 
-	console.log("After");
-	print_ap(ap);
+	main_stage.addEventListener("stagemousemove", ap_highlight);
+	main_stage.addEventListener("stagemousedown", ap_start);
+});
+
+function ap_highlight(event)
+{
+	hover_hex = get_hex_under_point({x: event.stageX, y:event.stageY});
+	
+	if(hover_hex != null)
+	{
+		if(current_highlighted != null && hover_hex != current_highlighted && current_highlighted != selected_one)
+			current_highlighted.unHighlight();
+		current_highlighted = hover_hex;
+		current_highlighted.highlight();
+	}
+	else
+	{
+		if(current_highlighted != null && current_highlighted != selected_one)
+			current_highlighted.unHighlight();
+		current_highlighted = null;
+	}
+}
+
+function ap_start()
+{
+	if(current_highlighted != null && selected_one == null)
+	{
+		selected_one = current_highlighted;
+	}
+	else if(current_highlighted != null)
+	{
+		selected_two = current_highlighted;
+
+		main_stage.removeEventListener("stagemousemove", ap_highlight);
+		main_stage.removeEventListener("stagemousedown", ap_start);
+
+		$("#info_panel").html("");
+		var p = path(selected_one.graph_index, selected_two.graph_index);	
+		if(p == "NO PATH")
+			$("#info_panel").html("There is no path between the two points");
+		draw_path(selected_one, selected_two, p);
+	}
+	else
+	{
+		$("#info_panel").html(prims_info + "Please click on a hex.</br>" + prims_button);
+		return false;
+	}
+}
+
+function draw_path(start, end, path)
+{
+	path_index = path.split(",");
+	var index_current = start.graph_index;
+	var index_next;
+	var edge;
+
+	for(var i = 1; i < path_index.length - 1; i ++)
+	{
+		index_next = path_index[i];
+		edge = new HexEdge();
+		edge.from = graph.hexes[index_current];
+		edge.to = graph.hexes[index_next];
+		edge.calculate_distance();
+		edge.draw_directed("black");
+		placed_edges.push(edge);
+		main_stage.addChild(edge.shape);
+		index_current = index_next;
+	}
+
+	index_next = end.graph_index;
+	edge = new HexEdge();
+	edge.from = graph.hexes[index_current];
+	edge.to = graph.hexes[index_next];
+	edge.calculate_distance();
+	edge.draw_directed("black");
+	placed_edges.push(edge);
+	main_stage.addChild(edge.shape);
+	index_current = index_next;
+}
+
+function path(i, j)
+{
+	if(ap.weight[i][j] >= LARGE_INT)
+		return "NO PATH";
+	var intermediate = next.weight[i][j];
+	if(intermediate == null)
+		return ",";
+	else 
+		return path(i, intermediate) + intermediate + path(intermediate, j);
 }
 
 function print_ap(ap)
@@ -65,15 +160,16 @@ function print_ap(ap)
 	}
 }
 
-function ap_struct(nverticies)
+function ap_struct(nverticies, default_val)
 {
 	this.weight = [];
 	this.nverts = nverticies;
+	default_val = default_arg(default_val, LARGE_INT)
 
 	for(var i = 0; i < this.nverts; i ++)
 	{
 		this.weight.push([]);
 		for(var j = 0; j < this.nverts; j ++)
-			this.weight[i].push(LARGE_INT);
+			this.weight[i].push(default_val);
 	}
 }
